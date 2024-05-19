8886548443bf10843dba01c739f502e6a7435c16
sanika Nadgauda <sanikanadgauda6@gmail.com>
2024-05-19 20:19:33 +0530

    image add

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from "react";
+import "./products.css";
 
 const Products = () => {
-    const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     fetch('https://my-json-server.typicode.com/SanikaNadgauda6/ecommerce-api/products')
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setProducts(data);
       });
   }, []);
 
-    return (
-        <>
-            <h2>
-                Products Page
-            </h2>
-            {/* {products.map((product) => (
-                <p>{product}</p>
-            ))} */}
-        </>
-    )
+  return (
+    <>
+      <h2 className="title">Products Page</h2>
+      <div className="products-container">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <h3 className="product-name">{product.name}</h3>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">${product.price.toFixed(2)}</p>
+            <p className="product-stock">Stock: {product.stock}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Products;
